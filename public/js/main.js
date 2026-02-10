a389152f11925a39a248c05af8b70cd311c4c33f
Martin Gleason <marty.data.scientist@icloud.com>
2026-02-10 15:33:25 -0600

    spinning and check functions
    spinning and some of the checking functions have been implemented.

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+//JS to rotate the image of Krampus.
+
+const submit = document.querySelector('#ask-krampus');
+
+submit.addEventListener("click", askKrampus);
+
+
+
+function askKrampus(event){
+    event.preventDefault();
+    console.log("Asking the great Krampus");
+
+    spinKrampus();
+
+    const question = takeQuestion();
+    const questionAsked = sanitizeQuesiton(question);
+    
+
+    const answer = document.getElementById("answer");
+    answer.textContent = questionAsked
+
+};
+
+function spinKrampus(){
+    const img = document.getElementById("joe-and-krampus");
+    img.classList.add('spinning');
+}
+
+function takeQuestion(){
+    const questionField = document.getElementById("question-id");
+    const question = questionField.value;
+    return(question)
+}
+
+function sanitizeQuesiton(question){
+    const defaultMessage = "No question was asked of the great Krampus!"
+    return question?.trim() ? question : defaultMessage;
+}
+
+function postAnswer(questionAsked){
+    
+}
